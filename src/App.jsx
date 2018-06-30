1d94d68519fa6d60d734f6941cd130d8dda9f6a8
James Leith <james@Jamess-MacBook-Pro.local>
2018-06-30 15:17:45 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from "./Components/Header";
+
+const App = () => (
+
+  <React.Fragment>
+
+    <Header />
+
+
+  </React.Fragment>
+
+
+
+);
+
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => (
+
+	<React.Fragment>
+
+		<header>
+
+			<h1>5 a side team picker</h1>
+
+		</header>
+
+	</React.Fragment>
+
+
+);
+
+export default Header; 
